b1c28854d60c700823c239133958fc4772ccc4cd
GenevievePlanteBrisebois <gen450@gmail.com>
2020-05-25 16:15:13 -0400

    Adding enemy movement and boundary check

--- RogueFrog/Assets/Scripts/Enemy.cs
+++ RogueFrog/Assets/Scripts/Enemy.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    private float speed = -1f;
+
+    //setters
+    public void setSpeed(float s) {
+        speed = s;
+    }
+
+    //getters
+    public float getSpeed()
+    {
+        return speed;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,22 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveEnemy();
+    }
+
+    //function for enemy movement
+    private void moveEnemy() {
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+
+        if (transform.position.x > (Screen.width / 100f) / 2f + 1f) {
+            transform.position = new Vector2(-transform.position.x+1f, transform.position.y);
+                
+                }
+        else if (transform.position.x < -(Screen.width / 100f) / 2f -1f) {
+            transform.position = new Vector2(-transform.position.x - 1f, transform.position.y);
+
+        }
+
     }
 }
